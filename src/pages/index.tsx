6ae4ba4ffadc789e1f1a3187bb13fce34a2e9fd1
Thao Bui <thuthao896@gmail.com>
2022-03-20 23:32:11 +0000

    set up

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return <div className="font-bold text-3xl">test</div>;
+};
+
+export default Home;
