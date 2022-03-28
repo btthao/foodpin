c03e8a2682ff2124771fb2a4b8cdc8da879174bc
Thao Bui <thuthao896@gmail.com>
2022-03-28 14:30:26 +0100

    init chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,30 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  semanticTokens: {
+    colors: {
+      error: "red.500",
+      success: "green.500",
+      primary: {
+        default: "orange.500",
+      },
+      secondary: {
+        default: "yellow.400",
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </Provider>
   );
 }
